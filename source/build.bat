00076425d11e387c93f71ea1bbcac11f82e7be0e
shootthesound <110498357+shootthesound@users.noreply.github.com>
2026-07-07 01:48:01 +0100

    Windows exe gets the Mac icon (zig rc resource); MIT license

--- source/build.bat
+++ source/build.bat
@@ -1,3 +1,4 @@
 @echo off
-rem Build iRate.exe (needs zig on PATH, or: pip install ziglang & use "python -m ziglang c++")
-zig c++ -target x86_64-windows-gnu -O2 -municode -DUNICODE -D_UNICODE -w irate.cpp -o ..\iRate.exe -Wl,--subsystem,windows -lole32 -luser32 -lgdi32 -lshell32 -lshlwapi -lmsimg32
+rem Build iRate.exe (needs zig on PATH, or: pip install ziglang & use "python -m ziglang c++" / "python -m ziglang rc")
+zig rc /:output-format coff irate.rc irate_res.obj
+zig c++ -target x86_64-windows-gnu -O2 -municode -DUNICODE -D_UNICODE -w irate.cpp irate_res.obj -o ..\iRate.exe -Wl,--subsystem,windows -lole32 -luser32 -lgdi32 -lshell32 -lshlwapi -lmsimg32
